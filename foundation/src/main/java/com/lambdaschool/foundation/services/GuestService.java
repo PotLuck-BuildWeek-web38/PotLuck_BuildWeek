353d9b7c4af72c7d19361d8b61657a704ba2e950
ginabethrussell <ginabeth.russell@gmail.com>
2021-02-27 16:15:33 -0500

    created potluckguestcontroller routes and guestService and implementations to get guests, add a guest, or remove a guest

--- foundation/src/main/java/com/lambdaschool/foundation/services/GuestService.java
+++ foundation/src/main/java/com/lambdaschool/foundation/services/GuestService.java
@@ -1,5 +1,15 @@
 package com.lambdaschool.foundation.services;
 
+import com.lambdaschool.foundation.models.Potluck;
+import com.lambdaschool.foundation.models.User;
+
+import java.util.List;
+
 public interface GuestService
 {
+    List<User> findAllGuests(long id);
+
+    User addGuest(long potluckid, long userid);
+
+    User removeGuest(long potluckid, long userid);
 }
